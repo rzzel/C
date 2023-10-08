241404ad0d469fcdd2762c787bc2227aca7874de
Ruzzel <123532716+rzzel@users.noreply.github.com>
2023-10-09 00:24:39 +0800

    Add files via upload

---
+++ seatwork David,Ruzzel-1.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std; 
+
+int main ()
+{
+	
+float num;
+float x,y;
+float mul;
+//float 
+
+cout << "This program is used to determine the cost of the 4K ULTRA HD TV of Adisson O'Reilley \n";
+cout << "\n";
+cout << "Enter the sale price: ";
+ cin >> x;
+
+    cout << "\n You entered: " << x;    
+    cout << " \n Enter the sales tax rate: ";
+    cin >> y;
+    cout << "You entered: " << y;
+    cout << "x * y= " << x * y << endl;
+    
+	cout << "Answer: " <<x * y;
+    cout << "Sale price + Sales Tax Rate = " << x+z;
+    
+    cout << "Cost: " << x + z << endl;
+    
+    
+return 0;
+
+}
+
